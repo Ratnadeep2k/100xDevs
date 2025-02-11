a6c6c3b3795ba34611cb693f32faa69001105efc
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2025-02-12 00:26:21 +0530

    Add background gradient to Dashboard component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import React from "react"
 
 export const Dashboard = () => {
     return <div>
+       <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
         <Appbar />
         <div className="m-8">
             <Balance value={"10,000"} />
